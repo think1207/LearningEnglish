58cb68c3d0a2d7bc771e16b567e02d6c107b2745
Ryoma Sato <think.twice.r.s09@gmail.com>
2026-02-07 21:58:14 +0900

    Replace duplicated function to latest one

--- lib/widgets/word_card.dart
+++ lib/widgets/word_card.dart
@@ -59,7 +59,7 @@ class StaticCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(24),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.1),
+            color: Colors.black.withValues(alpha: 0.1),
             blurRadius: 20,
             offset: const Offset(0, 10),
           ),
